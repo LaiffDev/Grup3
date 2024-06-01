2dd64c27faf2a3dd535f826a6cf7be8833c69c20
LaiffDev <laiffdev@gmail.com>
2024-06-01 22:19:11 +0200

    aggiunto bottone indietro e formgroup per registrazione

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,20 +11,28 @@ import { HttpClient } from '@angular/common/http';
 export class RegisterComponent {
   hide = true;
 
+  //FORMGROUP per la registrazione dell'utente
+  RegisterUserForm = new FormGroup({
+    full_name : new FormControl('',[Validators.required]),
+    phone_number : new FormControl('',[Validators.required]),
+    cod_fisc : new FormControl('',[Validators.required]),
+    email : new FormControl('',[Validators.required]),
+    secret : new FormControl('',[Validators.required])
+  })
+
   constructor(private router: Router, private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.saveUserDetails()
+
   }
 
   saveUserDetails() {
-    this.http.get<any[]>('http://localhost:3000/api/users').subscribe((response) => {
-      console.log(response)
-    })
+    const full_name = this.RegisterUserForm.get('full_name').value
+    const phone_number = this.RegisterUserForm.get('phone_number').value
+    const cod_fisc = this.RegisterUserForm.get('cod_fisc').value
+    const email = this.RegisterUserForm.get('email').value
+    const secret = this.RegisterUserForm.get('secret').value
 
-
-
-    //alert('user saved. now changing to car registration');
-    //this.router.navigate(['car-register']);
+    console.log(`Username : ${full_name} | Phone Number : ${phone_number} |Codice Fiscale : ${cod_fisc} |Email : ${email} |Password : ${secret}`)
   }
 }
