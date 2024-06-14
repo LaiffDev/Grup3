1c176fe3dcf01e20b532516ee0f7f56754fbb3f0
mrskrsm <francescoforgiarini03@gmail.com>
2024-06-14 11:29:33 +0200

    welfihowa

--- backend/migrations/20240614085158-create-car.js
+++ backend/migrations/20240614085158-create-car.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('car', {
+    await queryInterface.createTable('cars', {
       plate: {
         allowNull: false,
         primaryKey: true,
