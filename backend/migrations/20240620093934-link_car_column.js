059bc71e654cfc45cc7c73d548a080bdb0f2609e
mrskrsm <francescoforgiarini03@gmail.com>
2024-06-20 11:55:35 +0200

    dioc

---
+++ backend/migrations/20240620093934-link_car_column.js
@@ -0,0 +1,39 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('car_recharge_columns', {
+      car_plate: {
+        type: Sequelize.STRING(7),
+        references: {
+          model: 'cars',
+          key: 'plate'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        primaryKey: true
+      },
+      column_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'recharge_column',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        primaryKey: true
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('car_recharge_columns');
+  }
+}
--- backend/models/recharge_column.js
+++ backend/models/recharge_column.js
@@ -3,29 +3,31 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class car extends Model {
+  class recharge_column extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      car.belongsToMany(models.recharge_column, {
+      recharge_column.belongsToMany(models.Car, {
         through: 'link_car_column',
-        foreignKey: 'car_plate',
-        otherKey: 'column_id'
+        foreignKey: 'column_id',
+        otherKey: 'car_plate'
       })
     }
   }
-  car.init({
-    plate: DataTypes.STRING(7),
-    manufacturer: DataTypes.STRING,
-    model: DataTypes.STRING,
-    owner_id: DataTypes.INTEGER
+  recharge_column.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    plus_code: DataTypes.STRING,
+    
   }, {
     sequelize,
-    modelName: 'car',
+    modelName: 'recharge_column',
     underscored: true,
   });
-  return car;
+  return recharge_column;
 };
