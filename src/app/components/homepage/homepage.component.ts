dd26c102f35f7d5a7ecf66242de0614cf89e6eb2
mrskrsm <francescoforgiarini03@gmail.com>
2024-06-26 16:27:00 +0200

    push

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -35,7 +35,7 @@ export class HomepageComponent {
       this.userID = localStorage.getItem('userID');
       console.log(this.userID)
     }
-    this.retrieveCar()
+    //this.retrieveCar()
   }
 
     /***********************
