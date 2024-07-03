e9dd407f23daaadf980c59e5ddc5c120b4dd9d64
LaiffDev <laiffdev@gmail.com>
2024-07-03 14:44:47 +0200

    layout home page

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,9 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component} from '@angular/core';
 import { User } from '../../models/user';
 import { CarRegistrationService } from '../../services/car-registration.service';
-import { Car } from '../../models/car';
-import { CarRegisterComponent } from '../car-register/car-register.component';
-import { MatDialog } from '@angular/material/dialog';
 
 
 @Component({
@@ -18,8 +15,9 @@ export class HomepageComponent {
 
   user: User | undefined;
   userID: string | null = null;
-  isMacchinaInCarica = false
-  percentualeMacchina = 0
+  isMacchinaInCarica: boolean = false
+  percentualeMacchina: number = 0
+  intervalId: any
 
   carModels = {
     i3 : '../../../assets/images/bwm-i3.png',
@@ -44,18 +42,32 @@ export class HomepageComponent {
    ************************/
     public caricaMacchina() {
       this.isMacchinaInCarica = !this.isMacchinaInCarica;
-      const intervalId = setInterval(() => {
-          this.percentualeMacchina += 1;
-          if (this.percentualeMacchina >= 100) {
-              this.percentualeMacchina = 100; // Ensures it does not exceed 100
-              clearInterval(intervalId); // Stop the interval
-          }
-      }, 3000);
+
+    if (this.isMacchinaInCarica) {
+      this.intervalId = setInterval(() => {
+        this.percentualeMacchina += 1;
+        if (this.percentualeMacchina >= 100) {
+          this.percentualeMacchina = 100; // Ensures it does not exceed 100
+          this.stopCarica();
+        }
+      }, 500);
+    } else {
+      this.stopCarica();
+    }
+
+      
+
   }
 
-    /***********************
-   * FUNCTION TO GET CAR
-   ************************/
+  //STOP AL CARICAMENTO DELLA MACCHINA
+  public stopCarica() {
+    this.percentualeMacchina = this.percentualeMacchina;
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+    this.isMacchinaInCarica = false;
+  }
+
+  //FUNZIONE PER AVERE DATI DI UNA MACCHINA SPECIFICA
   public retrieveCar(){
     this.carService.RetrieveCar(this.userID).subscribe({
       next:(res) => {
